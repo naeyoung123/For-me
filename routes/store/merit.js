d39f8fd174897346847fa0d19e27e04e9b4d85a2
naeyoung123 <naeyoung123@naver.com>
2021-12-16 22:45:14 +0900

    [FEAT] create merit.js

---
+++ routes/store/merit.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var router = express.Router();
+var template = require('../../lib/template.js');
+
+router.get('/store/merit', function (request, response) {
+    var title = '제품별장단점';
+    var head = `
+    `;
+    var body = `
+    <main class="flex-shrink-0">
+    <div class="container">
+    <br>
+    <div class="row">
+        <div class="col-md-6" style = "border : solid gray 1px; "> 
+            <img src='/images/pad.png' alt="일회용 월경대" width = "100%">
+            <center><a href="pad"><button type="button" class="btn btn-secondary">일회용 월경대 제품 보러가기</button></a></center><br>
+            </div>
+        <div class="col-md-6" style = "border : solid gray 1px;"> 
+            <img src='/images/cpad.png' alt="면 월경대" width = "100%" >
+            <center><a href="cpad"><button type="button" class="btn btn-secondary">면 월경대 제품 보러가기</button></a></center><br>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-6" style = "border : solid gray 1px;"> 
+            <img src='/images/cup.png' alt="월경컵" width = "100%" >
+            <center><a href="cup"><button type="button" class="btn btn-secondary">월경컵 제품 보러가기</button></a></center><br>
+        </div>
+        <div class="col-md-6" style = "border : solid gray 1px;"> 
+            <img src='/images/tampon.png' alt="탐폰" width = "100%" >
+            <center><a href="tampon"><button type="button" class="btn btn-secondary">탐폰 제품 보러가기</button></a></center><br>
+        </div>
+    </div>
+    <br><br>
+    </main>
+`;
+    var html = template.HTML(title, head, body);
+    response.send(html);
+});
+
+module.exports = router;
