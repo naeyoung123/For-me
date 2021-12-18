b403e443f48f5ae1d4cbcb15174491f5d65614b2
jeayoungKim529 <jeay529@gmail.com>
2021-12-18 14:11:35 +0900

    [FEAT] 상세보기 구현

---
+++ routes/community/page_rec.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var router = express.Router();
+var template = require('../../lib/template.js');
+const db = require('../../db.js');
+var path = require('path');
+
+router.get('/community/recommendation/page/:pageId', function (request, response) {
+    var pageId = path.parse(request.params.pageId).base;
+    db.query(`SELECT * FROM recommendation WHERE id=?`, [pageId], function(err, res){
+        var title = '추천글보기';
+        var head = `
+            <style>
+                @font-face {
+                    font-family: 'NanumSquareRound';
+                    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_two@1.0/NanumSquareRound.woff') format('woff');
+                    font-weight: normal;
+                    font-style: normal;
+                }
+                *{
+                    font-family: 'NanumSquareRound';
+                }
+                .recommendation_page{
+                    margin-top:200px;
+                }
+            </style>
+        `;
+        var body = `
+            <main class="flex-shrink-0">
+                <div class="recommendation_page">
+                    <p name="title">제목:${res[0].title}</p>
+                    <hr>
+                    <p name="content">내용:${res[0].content}</p>
+                </div>
+            </main>
+        `;
+        var html = template.HTML(title, head, body);
+        response.send(html);
+    });
+});
+
+module.exports = router;
