75cf64b703593774f3e808916eb407946a923e4c
jeayoungKim529 <jeay529@gmail.com>
2021-12-18 14:08:15 +0900

    [FEAT] community main 구현

---
+++ routes/community/main_req.js
@@ -0,0 +1,61 @@
+var express = require('express');
+var router = express.Router();
+var template = require('../../lib/template.js');
+const db = require('../../db.js');
+
+router.get('/community/requirement', function (request, response) {
+    db.query(`SELECT * FROM requirement`, function(err, res){
+        var title = 'FORME에게 바란다.';
+        var head = `
+            <style>
+                @font-face {
+                    font-family: 'NanumSquareRound';
+                    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_two@1.0/NanumSquareRound.woff') format('woff');
+                    font-weight: normal;
+                    font-style: normal;
+                }
+                *{
+                    font-family: 'NanumSquareRound';
+                }
+                .recommend_main{
+                    margin-top:200px;
+                }
+            </style>
+        `;
+        var list = '';
+        var i = 0;
+        while(i < res.length) {
+            list += `
+                <tr>
+                    <td>${res[i].id}</td>
+                    <td>
+                        <a href="/community/requirement/page/${res[i].id}">${res[i].title}</a>
+                    </td>
+                    <td>${res[i].writer}</td>
+                    <td>${res[i].date}</td>
+                </tr>
+            `;
+            i++;
+        }
+        var body = `
+            <main class="flex-shrink-0">
+                <div class="recommend_main">
+                    <a href="/community/create_req">글쓰기</a>
+                    <table>
+                    <tr>
+                        <td>No.</td>
+                        <td>Title.</td>
+                        <td>Writer</td>
+                        <td>Date</td>
+                    </tr>
+                        ${list}
+                    </table>
+                </div>
+            </main>
+        `;
+        var html = template.HTML(title, head, body);
+        response.send(html);
+    })
+});
+
+module.exports = router;
