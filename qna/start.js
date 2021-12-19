ba175e136816ac929f9141c32992e2f0e043231f
gainlee <55633213+LGAIN@users.noreply.github.com>
2021-12-19 15:06:56 +0900

    qnalist
    결과 연결 전 ver

---
+++ qna/start.js
@@ -0,0 +1,49 @@
+const main = document.querySelector("#main");
+const qna = document.querySelector("#qna");
+const endPoint = 7;
+
+function addAnswer(answerText, qIdx){
+    var a = document.querySelector('.answerBox');
+    var answer = document.createElement('button');
+    answer.classList.add('answerList');
+    a.appendChild(answer);
+    answer.innerHTML = answerText;
+
+    answer.addEventListener("click", function(){
+        var children = document.querySelectorAll('.answerList');
+        for(let i = 0; i < children.length; i++){
+            children[i].disabled = true;
+            children[i].style.display = 'none';
+        }
+        goNext(++qIdx);
+    }, false);
+
+}
+
+function goNext(qIdx){
+    if(++qIdx === endPoint){
+        goResult()
+    }
+    var q = document.querySelector('.qBox');
+    q.innerHTML = QnAList[qIdx].q;
+    for(let i in QnAList[qIdx].a){
+        addAnswer(QnAList[qIdx].a[i].answer, qIdx);
+    }
+    var status = document.querySelector('.statusBar');
+    status.style.width = (100/endPoint) * (qIdx+1) + '%';
+}
+
+function begin(){
+    main.style.WebitAnimation = "fadeOut 1s";
+    main.style.animation = "fadeOut 1s";
+    setTimeout(() => {
+        qna.style.WebitAnimation = "fadeIn 1s";
+        qna.style.animation ="fadeIn 1s";
+        setTimeout (() => {
+            main.style.display = "none";
+            qna.style.display = "block";
+        }, 450)
+        let qIdx = 0;
+        goNext(qIdx);
+    }, 450);
+}
